9dc75a703ed80bfdeb3290820bb4e6f3fef38641
keriyason <keriyason@lcad.edu>
2026-02-20 03:35:59 -0800

    kill zone
    feat kill zone + player respawn

---
+++ Assets/Scripts/KillZone.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KillZone : MonoBehaviour
+{
+    [SerializeField] Transform spawnPoint;
+
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.transform.CompareTag("Player"))
+            col.transform.position = spawnPoint.position;
+    }
+}
